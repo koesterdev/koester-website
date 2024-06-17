854a118aa7cadd49ac54ee27d0023a63630f107b
Mark Koester <mark.koester1@gmail.com>
2024-06-16 20:38:51 -0400

    Style experience

--- app/mark/page.tsx
+++ app/mark/page.tsx
@@ -1,54 +1,95 @@
+import { EB_Garamond } from 'next/font/google';
+import { clsx } from 'clsx';
 import resume from './resume.json';
+
+const Garamond = EB_Garamond({
+  subsets: ['latin'],
+});
 
 const MarkPage = () => {
   return (
-    <main className="min-h-screen">
-      <div className="mb-4">
-        <h1>Mark Koester</h1>
-      </div>
+    <main className={clsx('min-h-screen bg-gray-200 pt-8', Garamond.className)}>
+      <div className="mx-auto max-w-screen-lg rounded bg-white px-10 shadow-sm">
+        <h1 className="pt-8 text-center text-2xl font-semibold tracking-widest">
+          Mark Koester
+        </h1>
 
-      <section>
-        <h2>Projects</h2>
-        {resume.experience.map(
-          ({ content, company, position, location }, index) => (
-            <div key={index} className="mb-4">
-              <h3>{position}</h3>
-              <p>{company}</p>
-              <p>{location}</p>
-              <ul>
-                {content.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
+        <section>
+          <SectionHeading title="Experience" />
+          {resume.experience.map((position, index) => (
+            <div key={index} className="mt-2">
+              <Position {...position} />
             </div>
-          ),
-        )}
-        <ul></ul>
-      </section>
-      <section className="mb-4">
-        <h2>Education</h2>
-        <p>{resume.education.institution}</p>
-        <p>{resume.education.degree}</p>
-        <p>{resume.education.gpa}</p>
-      </section>
-      <section className="mb-4">
-        <h2>Technical Skills</h2>
-        <h3>Programming Languages</h3>
-        <ul>
-          {resume.skills.languages.map((skill) => (
-            <li key={skill}>{skill}</li>
           ))}
-        </ul>
-        <h3>Frameworks</h3>
-        <ul>
-          {resume.skills.frameworks.map((skill) => (
-            <li key={skill}>{skill}</li>
-          ))}
-        </ul>
-      </section>
+        </section>
+        <section className="mb-4">
+          <h2>Education</h2>
+          <p>{resume.education.institution}</p>
+          <p>{resume.education.degree}</p>
+          <p>{resume.education.gpa}</p>
+        </section>
+        <section className="mb-4">
+          <h2>Technical Skills</h2>
+          <h3>Programming Languages</h3>
+          <ul>
+            {resume.skills.languages.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+          <h3>Frameworks</h3>
+          <ul>
+            {resume.skills.frameworks.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </main>
   );
 };
+
+const SectionHeading = ({ title }: SectionProps) => {
+  return (
+    <h2 className="border-b border-b-gray-900 text-xl font-semibold uppercase tracking-widest">
+      {title}
+    </h2>
+  );
+};
+
+interface SectionProps {
+  title: string;
+}
+
+const Position = ({
+  position,
+  startDate,
+  endDate,
+  company,
+  location,
+  content,
+}: PositionProps) => {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h3 className="font-semibold">{position}</h3>
+        <p className="italic">
+          {startDate}–{endDate}
+        </p>
+      </div>
+      <div className="flex justify-between">
+        <p className="font-semibold">{company}</p>
+        <p className="italic">{location}</p>
+      </div>
+      <ul className="ms-4 mt-1 list-disc">
+        {content.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+type PositionProps = (typeof resume)['experience'][number];
 
 export const metadata = {
   title: 'About Mark Koester',
